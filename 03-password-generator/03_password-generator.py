9d62f15a120ab87e56f7e421250edf4a21c98379
Portalo <smiguezc01@iesarroyoharnina.es>
2024-10-12 17:16:37 +0200

    start project 03 password generator

---
+++ 03-password-generator/03_password-generator.py
@@ -0,0 +1,20 @@
+import random as rnd
+
+LETTERS = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o',
+            'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D',
+            'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S',
+            'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+NUMBERS = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+SYMBOLS = [ '_', '-', '@', '!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+
+
+
+
+
+if __name__ == "__main__":
+
+    len_password = int(input("Cuantas letras quieres: "))
+    num_symbols = int(input("Cuantos simbolos quieres: "))
+    num_numbers = int(input("Cuantos números quieres: "))
+
