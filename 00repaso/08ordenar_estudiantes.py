0e0c67b582d9823def8bdb74e0e71fcb0de735e1
Portalo <smiguezc01@iesarroyoharnina.es>
2024-10-12 16:25:02 +0200

    repaso 08 DONE

--- 00repaso/08ordenar_estudiantes.py
+++ 00repaso/08ordenar_estudiantes.py
@@ -57,6 +57,9 @@ estudiantes_mayor_80 = list(filter(lambda x: x[1]>80, estudiantes.items()))
 print(f"Estudiantes con puntuacion amyor 80\n{estudiantes_mayor_80}")
 
 # 3. Calcular el promedio de calificaciones usando reduce()
-
-
+sumatorio_notas = reduce(lambda sum, x: sum + x[1], estudiantes.items(), 0) 
+promedio_notas = sumatorio_notas / len(estudiantes)
+print(f"Promedio de calificaciones:\n{round(promedio_notas, 2)}")
 # 4. Encontrar el estudiante con la calificación más baja usando min() y una función lambda
+estudiante_menor_calificacion = min(estudiantes.items(), key= lambda x: x[1] )
+print(f"Menor calificacion:\n{estudiante_menor_calificacion}")
