1340cae55fc2bcf3d039f51b6d0496b0692a39dd
Portalo <smiguezc01@iesarroyoharnina.es>
2024-10-12 18:53:38 +0200

    Proyecto 03 password generator, Realizada 2 versiones

--- 03-password-generator/03_password-generator-v2.py
+++ 03-password-generator/03_password-generator-v2.py
@@ -7,6 +7,26 @@ LETTERS = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n',
 NUMBERS = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 SYMBOLS = [ '_', '-', '@', '!', '#', '$', '%', '&', '(', ')', '*', '+']
 
+def generateList(lista = None, cantidad=0)-> list:
+    lista_nueva= list()
+    if (not isinstance(lista, list)):
+        raise TypeError("Tiene que ser tipo lista (list)")
+    if lista:
+        for _ in range(1, cantidad + 1):
+            index = rnd.randint(0, len(lista)-1)
+            lista_nueva.append(lista[index])
+    return lista_nueva
+
+def desordenarLista(lista= None):
+    if (not isinstance(lista, list)):
+        raise TypeError("Tiene que ser tipo lista (list)")
+    if lista:
+        for _ in range(len(lista)):
+            index1 = rnd.randint(0, len(lista) -1)
+            index2 = rnd.randint(0, len(lista)-1)
+            aux = lista[index1]
+            lista[index1] = lista[index2]
+            lista[index2] = aux
 
 if __name__ == "__main__":
     num_letters = int(input("Cuantas letras quieres: "))
@@ -14,18 +34,15 @@ if __name__ == "__main__":
     num_numbers = int(input("Cuantos números quieres: "))
     
     password = list()
-    # Con sample() cogemos valores sin que se repuitan
-    # symbols = rnd.sample(SYMBOLS, num_symbols)
-    # letters = rnd.sample(LETTERS, num_letters)
-    # numbers = rnd.sample(NUMBERS, num_numbers)
-
-    symbols = [rnd.choice(SYMBOLS) for _ in range(num_symbols)]
-    letters = [rnd.choice(LETTERS) for _ in range(num_letters)]
-    numbers = [rnd.choice(NUMBERS) for _ in range(num_numbers)]
+   
+    letters = generateList(LETTERS, num_letters)
+    symbols = generateList(SYMBOLS, num_symbols)
+    numbers = generateList(NUMBERS, num_numbers)
 
     password.extend(letters)
     password.extend(symbols)
     password.extend(numbers)
-
-    rnd.shuffle(password)
+    
+    print(password)
+    desordenarLista(password)
     print(f"Su contraseña es: {''.join(password)}")
