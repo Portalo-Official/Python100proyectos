dd61ba9930c2332d2046b85fd1950489e7845170
Portalo <smiguezc01@iesarroyoharnina.es>
2024-10-13 22:39:11 +0200

    Start projecto 04

---
+++ 04-analisis-productos/productos.py
@@ -0,0 +1,26 @@
+#  producto , cantiad_vendida, precio
+productos_datos = [
+    """
+    nombre_producto, cantidad_vendida, precio
+    """
+    ("Laptop", 25, 1200),
+    ("Smartphone", 80, 800),
+    ("Tablet", 50, 600),
+    ("Monitor", 40, 300),
+    ("Teclado", 100, 50),
+    ("Ratón", 120, 40),
+    ("Cámara", 30, 900),
+    ("Impresora", 45, 150),
+    ("Parlantes", 60, 100),
+    ("Cargador", 200, 20),
+    ("Disco Duro", 150, 100),
+    ("Memoria USB", 300, 15),
+    ("Smartwatch", 70, 250),
+    ("Consola de Juegos", 35, 500),
+    ("Televisor", 20, 2000),
+    ("Router", 75, 120),
+    ("Auriculares", 90, 80),
+    ("Microondas", 22, 350),
+    ("Refrigerador", 15, 2200),
+    ("Lavadora", 10, 1800)
+]
